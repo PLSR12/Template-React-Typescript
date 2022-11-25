19c6f31adcc2595b06f9271a78793aeb4751885b
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-11-25 11:49:12 -0300

    [*] inittial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import GlobalStyle from 'common/styles/globalStyles'
+import Routes from 'routes'
+
+function App() {
+  return (
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+export function Home() {
+  return (
+    <>
+      <S.Container>
+        <h1>HOME</h1>
+      </S.Container>
+    </>
+  )
+}
